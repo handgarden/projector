f141d5d4d22a16246e7e09a2a940d72a11251cce
handgarden <sjungwon03@gmail.com>
2024-03-31 03:43:43 +0900

    chore: ApiModule 파일 이름 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ApiModule } from './api/ApiModue';
+import { ApiModule } from './api/Api.module';
 import { GqlModule } from './graphql/Gql.module';
 
 @Module({
