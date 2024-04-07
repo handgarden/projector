0a76fe6baf9c00e04433225a208b2f2f88946b6c
handgarden <sjungwon03@gmail.com>
2024-04-07 16:29:32 +0900

    fix: DefaultValidationMessage 파일 이름 수정

--- src/common/message/validation/DefaultValidation.meesage.ts
+++
@@ -1,6 +0,0 @@
-export enum DefaultValidationMessage {
-  LENGTH = '$constraint1자 이상 $constraint2자 이하로 입력해주세요',
-  IS_NOT_EMPTY = '값을 입력해주세요',
-  IS_STRING = '문자열을 입력해주세요',
-  IS_NUMBER = '숫자를 입력해주세요',
-}
---
+++ src/common/message/validation/DefaultValidation.message.ts
@@ -0,0 +1,7 @@
+export enum DefaultValidationMessage {
+  LENGTH = '$constraint1자 이상 $constraint2자 이하로 입력해주세요.',
+  IS_NOT_EMPTY = '값을 입력해주세요.',
+  IS_STRING = '문자열을 입력해주세요.',
+  IS_NUMBER = '숫자를 입력해주세요.',
+  ARRAY_MIN_SIZE = '배열에 $constraint개 이상 값을 넣어주세요.',
+}
