efecd5e0f31bfe5dd30a25e786dd8a7a8a19af2e
handgarden <sjungwon03@gmail.com>
2024-06-09 18:55:52 +0900

    fix: loginDto 생성자 추가

--- backend/src/user/application/port/dto/Login.dto.ts
+++ backend/src/user/application/port/dto/Login.dto.ts
@@ -1,4 +1,9 @@
 export class LoginDto {
   account: string;
   password: string;
+
+  constructor({ account, password }: { account: string; password: string }) {
+    this.account = account;
+    this.password = password;
+  }
 }
