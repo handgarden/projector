3e35814723a42d2fda7de9987c5d5e2522cbbfaa
handgarden <sjungwon03@gmail.com>
2024-06-09 16:49:05 +0900

    chore: codegen schema 경로 수정

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "../projector/schema.gql",
+  schema: "../backend/schema.gql",
   documents: "app/**/*.tsx",
   ignoreNoDocuments: true,
   generates: {
